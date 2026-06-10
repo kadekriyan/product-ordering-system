bb47bb1afac3b0c0af071654e2368bfd89baf290
Kadek Riyan Kusuma Putra <kadekriyan16@gmail.com>
2026-06-11 06:13:27 +0700

    feat: Admin route (web & api)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/orders', [OrderController::class, 'index']);
