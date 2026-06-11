3b30b5ee51ee0834256ee82f3a80384817c5c8e4
Kadek Riyan Kusuma Putra <kadekriyan16@gmail.com>
2026-06-11 07:57:05 +0700

    fix: route admin

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,3 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/orders', [OrderController::class, 'store']);
-Route::get('/admin/orders', [OrderController::class, 'index'])->name('admin.orders.index');
-Route::get('/admin/orders/{order}/edit', [OrderController::class, 'edit'])->name('admin.orders.edit');
-Route::put('/admin/orders/{order}', [OrderController::class, 'update'])->name('admin.orders.update');
-Route::delete('/admin/orders/{order}', [OrderController::class, 'destroy'])->name('admin.orders.destroy');
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('admin.orders.index');
 });
 
 Route::get('/admin/orders', [OrderController::class, 'index']);
+Route::get('/admin/orders', [OrderController::class, 'index'])->name('admin.orders.index');
+Route::get('/admin/orders/{order}/edit', [OrderController::class, 'edit'])->name('admin.orders.edit');
+Route::put('/admin/orders/{order}', [OrderController::class, 'update'])->name('admin.orders.update');
+Route::delete('/admin/orders/{order}', [OrderController::class, 'destroy'])->name('admin.orders.destroy');
